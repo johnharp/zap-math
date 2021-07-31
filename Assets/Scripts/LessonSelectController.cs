f038d9d8eeb088029d1d625c43b405b3e1d04837
John Harper <john@harper.link>
2021-07-30 17:30:00 -0700

    add blank card

---
+++ Assets/Scripts/LessonSelectController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LessonSelectController : MonoBehaviour
+{
+    public void HandleAddButton()
+    {
+
+    }
+
+    public void HandleSubtractButton()
+    {
+
+    }
+
+    public void HandleMultiplyButton()
+    {
+
+    }
+
+    public void HandleDivideButton()
+    {
+
+    }
+}
